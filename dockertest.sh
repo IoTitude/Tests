342ae735990223fa4cddd5111d0375ba49c54de6
Bowsse <jaakko.poyhonen@gmail.com>
2016-06-13 15:49:34 +0300

    Update dockertest.sh

--- dockertest.sh
+++ dockertest.sh
@@ -1,13 +1,2 @@
 
-#!/bin/bash
-
-/usr/bin/expect <<EOF
-rm -R test
-expect "$ "
-spawn git clone https://github.com/IoTitude/Tests.git   test
-expect "$ "
-cd test
-spawn robot tests.txt
-expect eof
-exit 0
-EOF
+#!/bin/
