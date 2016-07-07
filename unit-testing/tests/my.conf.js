ee3f1e5f2b0cff23bf622ae3108312acf67df5d9
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-07 12:34:44 +0300

    Update my.conf.js

--- unit-testing/tests/my.conf.js
+++ unit-testing/tests/my.conf.js
@@ -14,20 +14,17 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-  '../www/lib/angular/angular.js',
-  '../www/js/login/*.js',
+  '../www/lib/ionic/js/ionic.bundle.js',
+  '../www/lib/ionic/js/ionic.js',
   '../www/js/*.js',
   '../www/js/*/*.js',
   '../www/lib/angular-mocks/angular-mocks.js',
-  '**/*tests.js',
-  '../tests/*'
+  '../tests/*/*tests.js'
     ],
 
 
     // list of files to exclude
     exclude: [
-	  '../www/js/baasbox/*.js',
-	  '../www/js/error/*.js',
     ],
 
 
