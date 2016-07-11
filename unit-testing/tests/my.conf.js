c231dc9a63959f2597b8b79afba7cd13a36c014e
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-11 15:20:34 +0300

    Update my.conf.js

--- unit-testing/tests/my.conf.js
+++ unit-testing/tests/my.conf.js
@@ -37,7 +37,10 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'junit'],
+    junitReporter = {
+      outputFile: 'test-results.xml'
+    }
 
 
     // web server port
@@ -64,7 +67,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
