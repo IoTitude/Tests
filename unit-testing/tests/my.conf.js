50fda3b442e28bbec82fb1436ee7f082fb9dc4a0
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-11 15:32:28 +0300

    Update my.conf.js

--- unit-testing/tests/my.conf.js
+++ unit-testing/tests/my.conf.js
@@ -38,9 +38,10 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'junit'],
-    junitReporter = {
+    
+    junitReporter: {
       outputFile: 'test-results.xml'
-    }
+    },
 
 
     // web server port
